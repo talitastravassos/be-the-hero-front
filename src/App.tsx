6ede4d79bb2c62091e9b51faa918323fa9c4fe83
talita <talitatravassos@outlook.com>
2020-04-04 16:07:27 -0300

    added routes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.scss';
-import Login from './pages/Login';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-      <Login />
+      <Routes />
     </>
   );
 }
