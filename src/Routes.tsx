8978856f9ade1fc8092900d30aca292909e897fd
talita <talitatravassos@outlook.com>
2020-04-05 12:22:41 -0300

    added NewIncident page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import NewIncident from './pages/NewIncident';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 
@@ -11,6 +12,7 @@ export default function Routes() {
         <Route path='/' exact component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/profile' component={Profile} />
+        <Route path='/incidents/new' component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/NewIncident/index.tsx
@@ -0,0 +1,63 @@
+
+import React, { FormEvent, useState } from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import './styles.scss';
+
+export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  const handleNewIncident = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para resolver
+            isso.
+        </p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form onSubmit={handleNewIncident}>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Título do caso"
+          />
+          <textarea
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            placeholder="Descrição"
+          />
+          <input
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Valor em reais"
+          />
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
